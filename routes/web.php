bda6fdbeea851d1e067ca2f6e82f9d65095f66b7
hasta332 <hastaalik67@gmail.com>
2025-11-20 10:22:07 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ElectronicController;
 
+// Full View harus di atas resource!
+Route::get('/electronics/fullview', [ElectronicController::class, 'fullView'])
+    ->name('electronics.fullview');
 
-Route::get('/', [ElectronicController::class, 'index']);
 Route::resource('electronics', ElectronicController::class);
+
+// Optional: homepage ke index
+Route::get('/', [ElectronicController::class, 'index']);
